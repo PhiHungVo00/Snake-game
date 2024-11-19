c27c545f57875b3beae26017b7a4a4d36772f439
qcron <23520205@gm.uit.edu.vn>
2024-11-19 13:57:46 +0700

    logic

--- logic.py
+++ logic.py
@@ -1,8 +1,9 @@
 def move(x, y, dx, dy):
     return x + dx, y + dy
 
-def collide_wall(x, y, width, height):
-    return x >= width or x < 0 or y >= height or y < 0
+def collide_wall(x, y, width, height, border_thickness=5):
+    return x < border_thickness or x >= width - border_thickness or \
+           y < border_thickness or y >= height - border_thickness
 
 def collide_self(snake_list):
     head = snake_list[-1]
