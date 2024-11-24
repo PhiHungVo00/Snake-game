1689e372d0a2b4faa8e3c1babf92b1f546667041
23521451 <23521451@gm.uit.edu.vn>
2024-11-24 12:56:16 +0700

    Thay doi UI, logic va them highscore.txt de luu diem

--- game.py
+++ game.py
@@ -1,14 +1,14 @@
 import pygame  # Thư viện pygame để xử lý giao diện
 import random
-from ui import draw_snake, draw_food, draw_menu, draw_scoreboard
-from logic import move, collide_wall, collide_self, eat_food
+from ui import draw_snake, draw_food, draw_menu, draw_scoreboard, main_menu, save_high_scores, get_SNAKE_SPEED, get_GAME_MODE, get_SNAKE_COLOR
+from logic import move_classic, move_modern, collide_wall, collide_self, eat_food
 from music import play_background_music, play_eat_sound
+
 pygame.init()  # Khởi tạo pygame
 play_background_music()
 # Thiết lập kích thước màn hình và các biến game
 WIDTH, HEIGHT = 800, 600
 SNAKE_BLOCK = 20
-SNAKE_SPEED = 12
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
@@ -20,8 +20,10 @@ pygame.display.set_caption("Snake Game")
 clock = pygame.time.Clock()
 
 def game_loop():
+
     game_over = False
     game_close = False
+    game_start = False
     x, y = WIDTH // 2, HEIGHT // 2
     dx, dy = 0, 0
     snake_list = []
@@ -43,6 +45,10 @@ def game_loop():
                         game_close = False
                     if event.key == pygame.K_c:
                         game_loop()
+        
+        while not game_start:
+            game_start = main_menu()
+        
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -59,9 +65,22 @@ def game_loop():
                     dx, dy = 0, SNAKE_BLOCK
 
 
-        x, y = move(x, y, dx, dy)
-        if collide_wall(x, y, WIDTH, HEIGHT) or (len(snake_list) > 1 and collide_self(snake_list)):
-            game_close = True
+        SNAKE_SPEED = get_SNAKE_SPEED() 
+        GAME_MODE = get_GAME_MODE()
+        SNAKE_COLOR = get_SNAKE_COLOR()
+
+        if GAME_MODE == 'classic':
+            x, y = move_classic(x, y, dx, dy)
+            if (len(snake_list) > 1 and collide_self(snake_list)):
+                game_close = True
+                save_high_scores(length_of_snake-1)
+                game_start = False
+        else: 
+            x, y = move_modern(x, y, dx, dy)
+            if collide_wall(x, y, WIDTH, HEIGHT) or (len(snake_list) > 1 and collide_self(snake_list)):
+                game_close = True
+                save_high_scores(length_of_snake-1)
+                game_start = False
 
         screen.fill(BLACK)
         draw_food(screen, foodx, foody, SNAKE_BLOCK)
@@ -85,3 +104,6 @@ def game_loop():
     quit()
 
 game_loop()
+
+
+
